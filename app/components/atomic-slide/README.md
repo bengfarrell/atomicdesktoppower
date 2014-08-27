f2d8eea14022f46ee8cd2467a04ba1197e052fda
ben <ben@benfarrell.com>
2014-08-26 23:26:07 -0700

    initial

---
+++ app/components/atomic-slide/README.md
@@ -0,0 +1,8 @@
+atomic-slide
+================
+
+See the [component page](http://bengfarrell.github.io/atomic-slide) for more information.
+
+## Getting Started
+
+We've put together a [guide for atomic-slide](http://www.polymer-project.org/docs/start/reusableelements.html) to help get you rolling.
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var downloadatomshell = require('gulp-download-atom-shell');
+
+gulp.task('downloadatomshell', function(cb){
+    downloadatomshell({
+        version: '0.13.3',
+        outputDir: 'binaries'
+    }, cb);
+});
+
+gulp.task('demo', shell.task([
+    'binaries/atom app true'
+]));
+
+gulp.task('democ', shell.task([
+    'binaries/atom app true components/' + gulp.env.comp + '/demo.html'
+]));
+
+gulp.task('default', ['downloadatomshell']);
