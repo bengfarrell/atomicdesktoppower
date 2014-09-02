d08e0c89f12f75bd771d789d6db2c1d2d40e0822
ben <ben@benfarrell.com>
2014-09-01 20:54:52 -0700

    another slide, plus a gulp task to number the JSON slides. Lastly a small tweak for not allowing  numbering to go beyond or before slidecount

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
+var fs = require('fs');
 var downloadatomshell = require('gulp-download-atom-shell');
 
 gulp.task('downloadatomshell', function(cb){
@@ -7,6 +8,23 @@ gulp.task('downloadatomshell', function(cb){
         version: '0.13.3',
         outputDir: 'binaries'
     }, cb);
+});
+
+gulp.task('numberslides', function() {
+    var slideindex = 0;
+    var manifest = JSON.parse(fs.readFileSync('./app/slides/manifest.json'));
+    manifest.content.forEach( function(deck) {
+        try {
+            var slds = JSON.parse(fs.readFileSync('./app/slides/' + deck.file));
+            slds.slides.forEach(function(sld) {
+                sld.number = ++ slideindex;
+            });
+            fs.writeFileSync('./app/slides/' + deck.file, JSON.stringify(slds, null, 2));
+        } catch(e) {
+            console.log('JSON Parsing error for ./app/slides/' + deck.file);
+            console.log(e.message);
+        }
+    });
 });
 
 gulp.task('demo', shell.task([
